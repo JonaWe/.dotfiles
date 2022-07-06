04a24e0f94e23d261c242f1a0d9480a4f8a5feed
Jona Wessendorf <jonawessendorf@gmail.com>
2022-07-06 21:04:12 +0200

    adds dap config for js

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -144,6 +144,12 @@ require('packer').startup(function()
             })
         end,
     }
+
+    use 'mfussenegger/nvim-dap'
+    use 'rcarriga/nvim-dap-ui'
+    use 'theHamsta/nvim-dap-virtual-text'
+
+
     use {
         'JoosepAlviste/nvim-ts-context-commentstring',
         config = function()
@@ -325,6 +331,7 @@ require('nvim-treesitter.configs').setup({
         'toml',
         'tsx',
         'typescript',
+        'javascript',
         'vim',
         'vue',
         'yaml',
@@ -489,6 +496,61 @@ cmp.setup {
         { name = 'luasnip' },
     },
 }
+
+-- }}}
+
+
+-- Debugger {{{
+
+local dap = require("dap")
+local dapui = require("dapui")
+local daptext = require("nvim-dap-virtual-text")
+
+daptext.setup()
+
+dapui.setup()
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
+
+dap.adapters.node2 = {
+  type = 'executable',
+  command = 'node',
+  args = {os.getenv('HOME') .. '/dev/microsoft/vscode-node-debug2/out/src/nodeDebug.js'},
+}
+
+dap.configurations.javascript = {
+  {
+    name = 'Node',
+    type = 'node2',
+    request = 'launch',
+    program = '${file}',
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = 'inspector',
+    console = 'integratedTerminal',
+  },
+  {
+    -- For this to work you need to make sure the node process is started with the `--inspect` flag.
+    name = 'Attach to process',
+    type = 'node2',
+    request = 'attach',
+    processId = require'dap.utils'.pick_process,
+  },
+}
+
+vim.keymap.set('n', '<F5>', ':lua require"dap".continue()<CR>')
+vim.keymap.set('n', '<F6>', ':lua require"dap".step_over()<CR>')
+vim.keymap.set('n', '<F7>', ':lua require"dap".step_into()<CR>')
+vim.keymap.set('n', '<F8>', ':lua require"dap".step_out()<CR>')
+vim.keymap.set('n', '<leader>b', ':lua require"dap".toggle_breakpoint()<CR>')
 
 -- }}}
 
