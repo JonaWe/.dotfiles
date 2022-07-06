015d9fc23ef82dd5045a20e50f1a416d33a0e1ca
Jona Wessendorf <jonawessendorf@gmail.com>
2022-07-06 13:01:48 +0200

    adds comment plugin and configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -144,6 +144,66 @@ require('packer').startup(function()
             })
         end,
     }
+    use {
+        'JoosepAlviste/nvim-ts-context-commentstring',
+        config = function()
+            require'nvim-treesitter.configs'.setup {
+                context_commentstring = {
+                    enable = true
+                }
+            }
+        end
+    }
+
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup({
+                toggler = {
+                    line = 'gcc',
+                    block = 'gbc',
+                },
+                opleader = {
+                    line = 'gc',
+                    block = 'gb',
+                },
+                extra = {
+                    above = 'gcO',
+                    below = 'gco',
+                    eol = 'gcA',
+                },
+                mappings = {
+                    basic = true,
+                    extra = true,
+                    extended = false,
+                },
+                pre_hook = function(ctx)
+                    -- Only calculate commentstring for tsx filetypes
+                    if vim.bo.filetype == 'typescriptreact' then
+                        local U = require('Comment.utils')
+
+                        -- Determine whether to use linewise or blockwise commentstring
+                        local type = ctx.ctype == U.ctype.line and '__default' or '__multiline'
+
+                        -- Determine the location where to calculate commentstring from
+                        local location = nil
+                        if ctx.ctype == U.ctype.block then
+                            location = require('ts_context_commentstring.utils').get_cursor_location()
+                        elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+                            location = require('ts_context_commentstring.utils').get_visual_start_location()
+                        end
+
+                        return require('ts_context_commentstring.internal').calculate_commentstring({
+                            key = type,
+                            location = location,
+                        })
+                    end
+                end,
+            })
+            vim.api.nvim_set_keymap('n', '<C-_>', 'gcc', { noremap = true })
+            vim.api.nvim_set_keymap('v', '<C-_>', 'gb', { noremap = true })
+        end
+    }
 
     -- autocomplete
     use 'hrsh7th/nvim-cmp'
